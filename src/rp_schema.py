a45640dc56bb60b5c8047ecac82c122151195eb1
Jannis Baule <jannis.baule@googlemail.com>
2023-12-03 12:25:09 +0100

    short fix for test with input

--- src/rp_schema.py
+++ src/rp_schema.py
@@ -2,5 +2,18 @@ INPUT_SCHEMA = {
     'zip_url': {
         'type': str,
         'required': True
+    },
+    'instance_name': {
+        'type': str,
+        'required': True
+    },
+    'class_name': {
+        'type': str,
+        'required': True
+    },
+    'steps': {
+        'type': int,
+        'required': False,
+        'default': 30
     }
 }
